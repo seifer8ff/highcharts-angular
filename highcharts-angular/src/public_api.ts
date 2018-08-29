7101433d2dd47f3e15bd3e14f67b026cae394ae6
Brady Mauermann-Peterson <brady@tignis.com>
2018-08-28 21:59:24 -0700

    Removing ignored files

--- highcharts-angular/src/public_api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of highcharts-angular
- */
-
-export * from './lib/highcharts-chart.module';
-export * from './lib/highcharts-chart.component';
--- index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./src/app/highcharts-chart.component.ts')
